c4184a9474b544574fa27cc0f39f8f747314161c
Nailu07 <nailu0793@gmail.com>
2025-11-21 19:39:22 -0500

    Primer index de prueba

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Zefiro Aerolinea</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v6.7.2/css/all.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="icon" href="img/logoSL.png">
+    </head>
+
+    <body>
+        
+    </body>
+</html>
