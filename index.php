de2bc98997ee1276a593470742bf5235992f0394
Nailu07 <nailu0793@gmail.com>
2025-11-22 01:49:52 -0500

    Primer boceto de la pagina de inicio

--- index.php
+++ index.php
@@ -6,10 +6,22 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <link href="https://use.fontawesome.com/releases/v6.7.2/css/all.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-        <link rel="icon" href="img/logoSL.png">
+        <link rel="icon" href="img/logoAzul.png">
     </head>
 
     <body>
-        
+        <?php 
+        if(!isset($_GET["pid"])){
+            include ("presentacion/inicio.php");    
+        }/*else{
+            $pid = base64_decode($_GET["pid"]);
+            if(isset($_SESSION["id"])){
+                include ($pid);
+            }else{
+                include ($pid);
+                //TODO reparar esto
+            }
+        }*/
+        ?>       
     </body>
 </html>
---
+++ presentacion/inicio.php
@@ -0,0 +1,122 @@
+<nav class="navbar navbar-expand-lg position-relative">
+    <div class="w-100 h-100 position-absolute bg-primary"></div>
+    <div class="w-100 h-100 position-absolute bg-danger opacity-50"></div>
+
+    <div class="container position-relative">
+        <a class="navbar-brand"  href="<?php echo base64_encode('presentacion/inicio.php') ?>">
+            <img src="img/logoLetras.png" alt="logo Zefiro" height="50">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarNav" aria-controls="navbarNav"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">             
+                <li class="nav-item">
+                <a class="nav-link" href="#">Destinos</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Contacto y soporte</a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">    
+                <a class="btn btn-outline-primary text-white me-3" 
+                href="?pid=<?php echo base64_encode("presentacion/registroCliente.php")?>">
+                <i class="fa-solid fa-user-plus me-3"></i>Registrarse</a>
+                <a class="btn btn-outline-primary text-white" 
+                href="?pid=<?php echo base64_encode("presentacion/autenticar.php")?>">
+                <i class="fa-solid fa-user-check me-3"></i>Autenticarse</a>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container-fluid bg-primary bg-opacity-25">
+    <form class="d-flex py-3" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-primary" type="submit">Search</button>
+    </form>
+</div>
+
+<div class="container mx-2 mx-md-5 py-3">
+    <div class="card">
+        <div class="card-header bg-primary bg-opacity-50">
+            <h5 class="m-0">¡Se parte de la familia Zefiro!</h5>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-12 col-md-6 mb-3 mb-md-0 text-center py-5">
+                    <p>Prepara todo para tu viaje y registrate en nuestra pagina para comprar tus tiketes de manera facil y rapida.
+                        <br>¡No esperes más!<br><br>
+                        <a class="btn text-white" style="background-color: #6f42c1;"
+                        href="<?php echo base64_encode("presentacion/registroCliente.php")?>">
+                        <i class="fa-regular fa-address-card me-2"></i>Registrarte Ahora</a>
+                    </p>
+                </div>
+
+                <div class="col-12 col-md-6">
+                    <img src="img/patoRegistrate.png" alt="Imagen de registro" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container mx-2 mx-md-5 py-3">
+    <div class="card">
+        <div class="card-header bg-primary bg-opacity-50">
+            <h5 class="m-0">Conoce los vuelos mas economicos</h5>
+        </div>
+        <div class="card-body">
+            <h3>En desarrollo ...</h3>
+        </div>
+    </div>
+</div>
+
+<div class="container mx-2 mx-md-5 py-3">
+    <div class="card">
+        <div class="card-header bg-primary bg-opacity-50">
+            <h5 class="m-0">¿No sabes a donde ir de vacaciones? Estos son los lugares mas populares</h5>
+        </div>
+        <div class="card-body">
+            <h3>En desarrollo ...</h3>
+        </div>
+    </div>
+</div>
+
+
+<footer class="text-white py-3 position-relative">
+    <div class="w-100 h-100 position-absolute bg-primary"></div>
+    <div class="w-100 h-100 position-absolute bg-danger opacity-50"></div>
+    <div class="container position-relative">
+
+        <hr class="border-white mb-3">
+        <div class="d-flex justify-content-between align-items-center">
+
+            <div class="d-flex align-items-center gap-2">
+                <img src="img/logoLetras.png" alt="Logo Zefiro" height="40">
+                <p><strong>© 2025 Zefiro Aerolinea, Inc</strong></p>
+            </div>
+
+            <div class="d-flex align-items-center gap-3">
+                <a href="https://www.instagram.com/" target="_blank" class="text-white fs-4">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
+                <a href="https://www.facebook.com/" target="_blank" class="text-white fs-4">
+                    <i class="fa-brands fa-facebook-f"></i>
+                </a>
+                <a href="https://www.tiktok.com/" target="_blank" class="text-white fs-4">
+                    <i class="fa-brands fa-tiktok"></i>
+                </a>
+                <a href="https://co.linkedin.com/" target="_blank" class="text-white fs-4">
+                    <i class="fa-brands fa-linkedin-in"></i>
+                </a>
+            </div>
+
+        </div>
+    </div>
+</footer>
